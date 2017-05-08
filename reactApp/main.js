5157cdc3fe7303d93ad89e5cffa93db5e75cf7f2
OliveKong <2072446908@qq.com>
2017-05-08 16:29:50 +0800

    change some thing

--- reactApp/main.js
+++ reactApp/main.js
@@ -4,9 +4,11 @@ import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import PropDemo from './props.jsx';
 import StateDemo from './components/state.jsx';
+import SetstateDemo from './components/setState.jsx';
 
 ReactDOM.render(<App />, document.getElementById('app'));
-var title="OliveKong";
+var title = "OliveKong";
 // var title=123;
 ReactDOM.render(<PropDemo title={title}/>, document.getElementById('propdemo'));
-ReactDOM.render(<StateDemo/>, document.getElementById("statedemo"))
+ReactDOM.render(<StateDemo/>, document.getElementById("statedemo"));
+ReactDOM.render(<SetstateDemo/>, document.getElementById("setstatedemo"));
